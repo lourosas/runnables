812f11a6278f634979fca70765329c6fb39c19f2
lourosas <lourosas@gmail.com>
2022-04-01 16:29:20 -0700

    04-01-22-01

--- Holder.java
+++ Holder.java
@@ -6,16 +6,25 @@ import java.lang.*;
 import java.util.*;
 
 public class Holder{
-   int value;
+   static int value = 32;
 
+   static{
+      System.out.println("Holder static Initializer:  "+value);
+
+   };
+
+   /*
    {
       System.out.println("Holder Initializer:  "+value);
-   }
+      value = 38;
+      System.out.println("Holder Initializer:  "+value);
+   };
+   */
 
    ///////////////////////////////////////////////////////////////////
    /**/
    public Holder(){
-      this.value = -1;
+      //this.value = -1;
       System.out.print("Holder Constructor of no arguments:  ");
       System.out.println(value);
    }
