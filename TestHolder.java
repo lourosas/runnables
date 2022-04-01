72edc3b89ac09e6da06c5759b722d8011d437da1
Lou <lourosas@gmail.com>
2022-04-01 13:21:21 -0700

    04-01-22-00

---
+++ TestHolder.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+import rosas.lou.runables.Holder;
+
+public class TestHolder{
+   /**/
+   public static void main(String [] args){
+      new TestHolder();
+   }
+
+   public TestHolder(){
+      
+   }
+}
