452cfe56d3a024aba6195ee3efb70fa0a914723c
lourosas <lourosas@gmail.com>
2023-06-02 15:33:54 -0700

    0602-23-01

--- TestCoffeeMaker3.java
+++ TestCoffeeMaker3.java
@@ -16,7 +16,8 @@ public class TestCoffeeMaker3{
       Maker maker = new Maker();
       CoffeeMakerController controller = new CoffeeMakerController();
       controller.addModel(maker);
-      CoffeeMakerView view = new CoffeeMakerView("A Coffee Maker");
+      CoffeeMakerView view =
+                    new CoffeeMakerView("A Coffee Maker",controller);
       maker.addSubscriber(view);
       controller.addSubscriber(view);
    }
