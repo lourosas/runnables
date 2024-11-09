00d48ea47b086e8e258265782e14611e088fa62d
Lou Rosas <lourosas@gmail.com>
2024-11-09 15:41:45 -0700

    11-09-24-02

--- LaunchingMechanismJFrame.java
+++ LaunchingMechanismJFrame.java
@@ -30,7 +30,7 @@ import java.time.*;
 import myclasses.*;
 import rosas.lou.clock.*;
 
-public class LaunchingMechanismJFrame extends GenericJFrame{
+public class LaunchingMechanismJFrame extends GenericJInteractionFrame{
    ///////////////////////////Constructor/////////////////////////////
    public LaunchingMechanismJFrame(){
       this.setUpGUI();
