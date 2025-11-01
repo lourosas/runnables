f9a0c0e1f247dd22123603a06ee5ed8e79afd1ff
Lou Rosas <lourosas@gmail.com>
2025-11-01 15:34:17 -0700

    11-01-25-00

--- GenericLaunchingMechanismDataFeeder.java
+++ GenericLaunchingMechanismDataFeeder.java
@@ -43,7 +43,91 @@ DataFeeder,Runnable{
    private LaunchStateSubstate.AscentSubstate    IGNE = null;
 
    //Read In
-   private LaunchingMec
+   private LaunchingMechanismData     _initMechData;
    //Calculated
+   private LaunchingMechanismData     _calcMechData;
+
+   private LaunchStateSubstate        _stateSubstate;
+   private Object                     _obj;
+   private Thread                     _t0;
+
+   private boolean                    _toStart;
+
+   {
+      INIT = LaunchStateSubstate.State.INITIALIZE;
+      PREL = LaunchStateSubstate.State.PRELAUNCH;
+      IGNI = LaunchStateSubstate.State.IGNITION;
+      LAUN = LaunchStateSubstate.State.LAUNCH;
+      ASCE = LaunchStateSubstate.State.ASCENT;
+      SET  = LaunchStateSubstate.PreLaunchSubstate.SET;
+      CONT = LaunchStateSubstate.PreLaunchSubstate.CONTINUE;
+      FUEL = LaunchStateSubstate.PreLaunchSubstate.FUELING;
+      HOLD = LaunchStateSubstate.PreLaunchSubstate.HOLD;
+      IGN  = LaunchStateSubstate.IgnitionSubstate.IGNITION;
+      BUP  = LaunchStateSubstate.IgnitionSubstate.BUILDUP;
+      STG  = LaunchStateSubstate.AscentSubstate.STAGING;
+      IGNE = LaunchStateSubstate.AscentSubstate.IGNITEENGINES; 
+
+      _initMechData   = null;
+      _calcMechData   = null;
+      _stateSubstate  = null;
+      _obj            = null;
+      _t0             = null;
+   };
+
+   ////////////////////////////Constructors///////////////////////////
+   //
+   //
+   //
+   public GenericLaunchingMechanismDataFeeder(){
+      this.setUpThread();
+   }
+
+   /////////////////////////Private Methods///////////////////////////
+   //
+   //
+   //
+   private void setUpThread(){
+      this._obj = new Object();
+      this._t0  = new Thread(this);
+      this._t0.start();
+   }
+
+   //////////////////////DataFeeder Implementation////////////////////
+   //
+   //
+   //
+   public void initialize(String file)throws IOException{}
+
+   //
+   //
+   //
+   public Object monitor(){
+      //Temp for now...
+      return null;
+   }
+
+   //
+   //
+   //
+   public void setStateSubstate(LaunchStateSubstate statesubstate){
+      this._stateSubstate = stateSubstate;
+   }
+
+   ///////////////////Runnable Interface Implementation///////////////
+   //
+   //
+   //
+   public void run(){
+      try{
+         while(true){
+            if(this._stateSubstate != null){
+               //Collect Data
+            }
+            Thread.sleep(1);
+         }
+      }
+      catch(InterruptedException ie){}
+   }
 }
 //////////////////////////////////////////////////////////////////////
