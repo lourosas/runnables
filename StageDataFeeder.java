2dde6b81fb8b6b1e9587a06d39012097b3fd2781
Lou Rosas <lourosas@gmail.com>
2025-12-09 16:09:43 -0700

    12-09-25-02

--- StageDataFeeder.java
+++ StageDataFeeder.java
@@ -27,6 +27,68 @@ import rosas.lou.runnables.*;
 import rosas.lou.clock.*;
 
 public class StageDataFeeder implements DataFeeder, Runnable{
+   private LaunchStateSubstate.State INIT              = null;
+   private LaunchStateSubstate.State PREL              = null;
+   private LaunchStateSubstate.State IGNI              = null;
+   private LaunchStateSubstate.State LAUN              = null;
+   private LaunchStateSubstate.State ASCE              = null;
+   private LaunchStateSubstate.PreLaunchSubstate SET   = null;
+   private LaunchStateSubstate.PreLaunchSubstate CONT  = null;
+   private LaunchStateSubstate.PreLaunchSubstate FUEL  = null;
+   private LaunchStateSubstate.PreLaunchSubstate HOLD  = null;
+   private LaunchStateSubstate.IgnitionSubstate  IGN   = null;
+   private LaunchStateSubstate.IgnitionSubstate  BUP   = null;
+   private LaunchStateSubstate.AscentSubstate    STG   = null;
+   private LaunchStateSubstate.AscentSubstate    IGNE  = null;
+
+   //Read In....
+   private StageData              _initStageData;
+   //Derived
+   private StageData              _calcStageData;
+
+   private LaunchStateSubstate    _stateSubstate;
+   private Object                 _obj;
+   private Thread                 _t0;
+
+   private FuelSystemData         _fuelSystemData;
+
+   private int                    _engines;
+   private int                    _stage;
+
+   {
+      INIT = LaunchStateSubstate.State.INITIALIZE;
+      PREL = LaunchStateSubstate.State.PRELAUNCH;
+      IGNI = LaunchStateSubstate.State.IGNITION;
+      LAUN = LaunchStateSubstate.State.LAUNCH;
+      ASCE = LaunchStateSubstate.State.ASCENT;
+      SET  = LaunchStateSubstate.PreLaunchSubstate.SET;
+      CONT = LaunchStateSubstate.PreLaunchSubstate.CONTINUE;
+      FUEL = LaunchStateSubstate.PreLaunchSubstate.FUELING;
+      HOLD = LaunchStateSubstate.PreLaunchSubstate.HOLD;
+      IGN  = LaunchStateSubstate.IgnitionSubstate.IGNITION;
+      BUP  = LaunchStateSubstate.IgnitionSubstate.BUILDUP;
+      STG  = LaunchStateSubstate.AscentSubstate.STAGING;
+      IGNE = LaunchStateSubstate.AscentSubstate.IGNITEENGINES;
+
+      _initStageData        = null;
+      _calcStageData        = null;
+      _stateSubstate        = null;
+      _obj                  = null;
+      _t0                   = null;
+      _fuelSystemData       = null;
+      _engines              = -1;
+      _stage                = -1;
+   };
+
+   ////////////////////////////Constructors///////////////////////////
+   //
+   //
+   //
+   public StageDataFeeder(int currentStage){
+      this.setStageNumber(currentStage);
+      this.setUpThread();
+   }
+
    ///////////////////////////Public Methods//////////////////////////
    //
    //
@@ -34,18 +96,22 @@ public class StageDataFeeder implements DataFeeder, Runnable{
    static public DataFeeder instance(){}
 
    //////////////////////////Private Methods//////////////////////////
-   ////////////////////////////Constructors///////////////////////////
    //
    //
    //
-   private RocketDataFeeder(){
-      this.setUpThread();
+   private void setStageNumber(int stage){
+      if(stage > 0){
+         this._stage = stage;
+      }
    }
-
    //
    //
    //
-   private void setUpThread(){}
+   private void setUpThread(){
+      this._obj     = new Object();
+      this._t0      = new Thread(this);
+      this._t0.start();
+   }
 
    /////////////////////DataFeeder Implementation/////////////////////
    //
