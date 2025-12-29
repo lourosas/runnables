137333a19365cd40763c08b9a8383f4aab47ffef
lourosas <lourosas@gmail.com>
2025-12-29 13:50:58 -0700

    12-29-25-01

--- GenericPump.java
+++ GenericPump.java
@@ -99,72 +99,45 @@ public class GenericPump implements Pump, Runnable{
    //
    //
    //
-   private String flowError(double flow){
-      double ul        = Double.NaN;
-      double ll        = Double.NaN;
-      double tolerance = Double.NaN;
-      String error     = null;
-      PumpData pd      = this.myPumpData();
-
+   private void initializePumpDataJSON(String file)throws IOException{
+      double flo  = Double.NaN; int idx = -1; int stg = -1;
+      double temp = Double.NaN; double tol = Double.NaN;
+      String type = null; String err = null; boolean isE = false;
+      //Test Print
+      System.out.println("Generic Pump: "+file);
       try{
-         tolerance   = pd.tolerance();
-         if(!Double.isNaN(flow) && !Double.isNaN(tolerance)){
-            //Separate out by State
-            if(this._state.state() == INIT){
-               //INIT there should be NO FLOW through the pump
-               //ANY FLOW through the pump is an error!!!
-               ll = tolerance - 1.;
-               ul = 1 - tolerance;
-            }
-            else if(this._state.state() == PRELAUNCH){}
-            if(flow < ll || flow > ul){
-               error  = new String("Stage "+pd.stage());
-               error += "Tank: "+pd.index()+" Pump Flow Error: ";
-               if(flow < ll){
-                  error += "too low";
-               }
-               else if(flow > ul){
-                  error += "too high";
-               }
+         LaunchSimulatorJsonFileReader read = null;
+         read = new LaunchSimulatorJsonFileReader(file);
+         List<Hashtable<String,String>> lst = read.readPumpDataInfo();
+         Iterator<Hashtable<String,String>> it = lst.iterator();
+         while(it.hasNext()){
+            Hashtable<String,String> ht = it.next();
+            try{ idx = Integer.parseInt( ht.get("tanknumber")); }
+            catch(NumberFormatException nfe){ idx = -1; }
+            try{ stg = Integer.parseInt( ht.get("stage")); }
+            catch(NumberFormatException nfe){ stg = -1; }
+            if(this._stage == stg && this._tank == idx){
+               try{ flo = Double.parseDouble(ht.get("rate")); }
+               catch(NumberFormatException nfe){ flo = Double.NaN; }
+               try{ temp = Double.parseDouble(ht.get("temperature")); }
+               catch(NumberFormatException nfe){ temp = Double.NaN; }
+               try{ tol = Double.parseDouble(ht.get("tolerance")); }
+               catch(NumberFormatException nfe){ tol = Double.NaN; }
+               this._pumpData = new GenericPumpData(
+                                              err,  //Error
+                                              flo,  //Rate
+                                              idx,  //Number
+                                              isE,  //Is Error
+                                              stg,  //Stage
+                                              temp, //Temp
+                                              tol,  //Tol
+                                              type);//Fuel Type
             }
          }
       }
-      catch(NullPointerException npe){
-         error  = new String(npe.getMessage());
-         error += "\nStage "+pd.stage()+" Tank "+pd.index();
-         error += " Pump Error Unknown";
-      }
-      finally{
-         return error;
-      }
-   }
-
-   //
-   //
-   //
-   private void isError(double flow, double temperature){
-      boolean isError  = false;
-      String  error    = new String();
-      String flowError = this.flowError(flow);
-      String tempError = this.temperatureError(temperature);
-      if(flowError != null){
-         error   += " " + flowError;
-         isError  = true;
-      }
-      if(tempError != null){
-         error  += " " + tempError;
-         isError = true;
-      }
-      if(isError){
-         int    idx  = this._measuredPumpData.index();
-         int    stg  = this._measuredPumpData.stage(); 
-         double tol  = this._measuredPumpData.tolerance();
-         String typ  = this._measuredPumpData.type();
-         PumpData pd = new GenericPumpData(error,flow,idx,isError,
-                                           stg,temperature,tol,typ);
-         synchronized(this._obj){
-            this._measuredPumpData = pd;
-         }
+      catch(IOException ioe){
+         this._pumpData = null;
+         throw ioe;
       }
    }
 
@@ -197,142 +170,10 @@ public class GenericPump implements Pump, Runnable{
       }
    }
 
-   //The Fuel temperature in the pump MUST be within rage
-   //REGARLESS of State!!!
-   //
-   //
-   private String temperatureError(double temp){
-      double ul        = Double.NaN;
-      double ll        = Double.NaN;
-      double tolerance = Double.NaN;
-      String error     = null;
-      PumpData pd      = this.myPumpData();
-      
-      try{
-         tolerance  = pd.tolerance();
-         if(!Double.isNaN(temp) && !Double.isNaN(tolerance)){
-            //Separate out by State
-            if(this._state.state() == INIT){
-               //Since there is nothing in the Tank..and thuns,
-               //nothing in the pump, no need to worry about tol
-               ul = 373.15; //Boiling Point of Water in K
-               ll = 273.15; //Freezing Point of Water in K
-            }
-            else if(this._state.state() == PRELAUNCH){}
-            if(temp < ll || temp > ul){
-               error  = new String("Stage "+pd.stage());
-               error += "Tank: "+pd.index();
-               error += " Pump Temerature Error: ";
-               if(temp < ll){
-                  error += " too low";
-               }
-               else if(temp > ul){
-                  error += " too high";
-               }
-            }
-         }
-      }
-      catch(NullPointerException npe){
-         error  = new String(npe.getMessage());
-         error += "\nStage "+pd.stage()+" Tank "+pd.index();
-         error += " Pump Error Unknown";
-      }
-      finally{
-         return error;
-      }
-   }
-
-   //The flow is measured in Liters/sec...
-   //
-   //
-   private double measureFlow(){
-      double flow = 0.;
-      try{
-         PumpData pd = this.myPumpData();
-         flow        = pd.flow();
-      }
-      catch(NullPointerException npe){
-         //Temporary until actual Hardware is available...
-         flow = this._pumpData.flow();
-      }
-      finally{
-         return flow;
-      }
-   }
-
-   //
-   //
-   //
-   private double measureTemperature(){
-      double temperature = Double.NEGATIVE_INFINITY;
-      try{
-         PumpData pd = this.myPumpData();
-         temperature = pd.temperature();
-      
-      }
-      catch(NullPointerException npe){
-         //Temporary until actual Hardware is available...
-         temperature = this._pumpData.temperature();
-      }
-      finally{
-         return temperature;
-      }
-   }
-
    //
    //
    //
    private void monitorPump(){
-      //Measure the Current Flow
-      double flow = this.measureFlow();
-      //Measure the Temperature
-      double temp = this.measureTemperature();
-      this.setUpPumpData(flow, temp);
-      this.isError(flow, temp);
-
-   }
-
-
-   //
-   //
-   //
-   private PumpData myPumpData()throws NullPointerException{
-      return null;
-      /*
-       * Need to redo to fit the new DataFeeder Interface
-      PumpData pumpData = null;
-      try{
-         RocketData            rd = this._feeder.rocketData();
-         List<StageData>     list = rd.stages();
-         Iterator<StageData>   it = list.iterator();
-         boolean            found = false;
-         while(!found && it.hasNext()){
-            StageData sd = it.next();
-            if(sd.stageNumber() == this._stage){
-               FuelSystemData     fsd    = sd.fuelSystemData();
-               List<PumpData>     pdList = fsd.pumpData();
-               Iterator<PumpData> p_it   = pdList.iterator();
-               while(p_it.hasNext() && !found){
-                  PumpData pd = p_it.next();
-                  if(pd.index() == this._tank){
-                     pumpData = pd;
-                     found    = true;
-                  }
-               }
-            }
-         }
-         if(!found){
-            throw new NullPointerException();
-         }
-      }
-      catch(NullPointerException npe){
-         pumpData = null;
-         throw npe;
-      }
-      finally{
-         return pumpData;
-      }
-      */
    }
 
    //
@@ -344,9 +185,7 @@ public class GenericPump implements Pump, Runnable{
          read = new LaunchSimulatorIniFileReader(file);
       }
       else if(file.toUpperCase().contains("JSON")){
-         LaunchSimulatorJsonFileReader read = null;
-         read = new LaunchSimulatorJsonFileReader(file);
-         this.setPumpData(read.readPumpDataInfo());
+         this.initializePumpDataJSON(file);
       }
    }
 
