2e681524bed54cf641d10f955854d0597efd07ad
Lou Rosas <lourosas@gmail.com>
2026-07-11 13:52:43 -0700

    07-11-26-00

--- GenericTankData.java
+++ GenericTankData.java
@@ -21,7 +21,7 @@ import java.lang.*;
 import java.util.*;
 import rosas.lou.runnables.*;
 
-public class GenericTankData implements TankData{
+public class GenericTankData extends TankData{
    ////////////////////////////Constructor////////////////////////////
    //
    //
--- TankInitializable.java
+++ TankInitializable.java
@@ -46,6 +46,111 @@ public class TankInitializable implements Initializable{
    //
    //
    //
+   private double getCapacity(Hashtable<String,String> ht){
+      double capacity = Double.NaN;
+      try{
+         capacity = Double.parseDouble(ht.get("capacity"));
+      }
+      catch(NumberFormatException nfe){
+         capacity = Double.NaN;
+      }
+      catch(NullPointerException npe){
+        npe.printStackTrace();
+        capacity = Double.NaN;
+      }
+      return capacity;
+   }
+
+   //
+   //
+   //
+   private double getDensity(Hashtable<String,String> ht){
+      double density = Double.NaN;
+      try{
+         density = Double.parseDouble(ht.get("density"));
+      }
+      catch(NumberFormatException nfe){
+         density = Double.NaN;
+      }
+      catch(NullPointerException npe){
+         npe.printStackTrace();
+         density = Double.NaN;
+      }
+      return density;
+   }
+
+   //
+   //
+   //
+   private double getDryWeight(Hashtable<String,String> ht){
+      double dryWeight = Double.NaN;
+      try{
+         dryWeight = Double.parseDouble(ht.get("dryweight"));
+      }
+      catch(NumberFormatException nfe){
+         dryWeight = Double.NaN;
+      }
+      catch(NullPointerException  npe){
+         npe.printStackTrace();
+         dryWeight = Double.NaN;
+      }
+      return dryWeight;
+   }
+
+   //
+   //
+   //
+   private double getEmptyRate(Hashtable<String,String> ht){
+      double emptyRate = Double.NaN;
+      try{
+         emptyRate = Double.parseDouble(ht.get("rate"));
+      }
+      catch(NumberFormatException nfe){
+         emptyRate = Double.NaN;
+      }
+      catch(NullPointerException  npe){
+         npe.printStackTrace();
+         emptyRate = Double.NaN;
+      }
+      return emptyRate;
+   }
+
+   //
+   //
+   //
+   private String getFuelType(Hashtable<String,String> ht){
+      String fuelType = null;
+      try{
+         fuelType = ht.get("fuel");
+      }
+      catch(NullPointerException npe){
+         npe.printStackTrace();
+         fuelType = null;
+      }
+      return fuelType;
+   }
+
+   //
+   //
+   //
+   private long getModelNumber(Hashtable<String,String> ht){
+      long model = -1;
+      try{
+         model = Long.parseLong(ht.get("model"),16);
+      }
+      catch(NumberFormatException nfe){
+         model = -1;
+      }
+      catch(NullPointerException  npe){
+         npe.printStackTrace();
+         model = -1;
+      }
+      return model;
+   }
+
+   //
+   //
+   //
    private int getHashtableNumber(Hashtable<String,String> ht){
       int number = -1;
       try{
@@ -110,15 +215,72 @@ public class TankInitializable implements Initializable{
    //
    //
    //
+   private double getTemperature(Hashtable<String,String> ht){
+      double temperature = Double.NaN;
+      try{
+         temperature = Double.parseDouble(ht.get("temperature"));
+      }
+      catch(NumberFormatException nfe){
+         temperature = Double.NaN;
+      }
+      catch(NullPointerException  npe){
+         npe.printStackTrace();
+         temperature = Double.NaN;
+      }
+      return temperature;
+   }
+
+   private double getTolerance(Hashtable<String,String> ht){
+      double tolerance = Double.NaN;
+      try{
+         tolerance = Double.parseDouble(ht.get("tolerance"));
+      }
+      catch(NumberFormatException nfe){
+         tolerance = Double.NaN;
+      }
+      catch(NullPointerException  npe){
+         npe.printStackTrace();
+         tolerance = Double.NaN;
+      }
+      return tolerance;
+   }
+
+   //
+   //
+   //
    private void initializeTank(String file)throws IOException{
       TankData td = null;
       LaunchSimulatorJsonFileReader read = null;
       read = new LaunchSimulatorJsonFileReader(file);
       List<Hashtable<String,String>> lst = read.readTankDataInfo();
       Hashtable<String,String> ht = this.getTankHashtable(lst);
-      System.out.println("++++++Tank Initializable+++++");
-      System.out.println(ht);
-      System.exit(0);
+      double cap = this.getCapacity(ht);
+      double den = this.getDensity(ht);
+      double dw  = this.getDryWeight(ht);
+      double er  = this.getEmptyRate(ht);
+      String fu  = this.getFuelType(ht);
+      double mlr = Double.NaN;  //Mass Loss Rate
+      long   mod = this.getModelNumber(ht);
+      int    num = this._number;
+      int    stg = this._stage;
+      double temp= this.getTemperature(ht);
+      double tol = this.getTolerance(ht);
+      double wgt = Double.NaN; //Measured Weight
+      td = new GenericTankData(cap,  //Capacity
+                               den,  //Density
+                               dw,   //Dry Weight
+                               er,   //Empty Rate
+                               null, //Error String
+                               fu,   //Fuel Type
+                               false,//Is Error
+                               mlr,  //Mass Loss Rate
+                               mod,  //Model
+                               stg,  //Stage
+                               num,  //Number
+                               temp, //Temperature
+                               tol,  //Tolerance
+                               wgt); //Weight
+      this._tankData = td;
    }
 
    //
